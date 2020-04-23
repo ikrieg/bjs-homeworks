5e002ceb2458ae2e484958e4e7428898ec8d27e7
Igor Sklyarov <i.sklyarov90@gmail.com>
2020-04-23 22:02:19 +0300

    add 1.2 solution

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,43 @@
+"use strict";
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x = [];
+
+    let d = Math.pow(b, 2) - 4 * a * c;
+
+    if (d > 0) {
+        x[0] = (-b + Math.sqrt(d)) / (2 * a);
+        x[1] = (-b - Math.sqrt(d)) / (2 * a);
+    }
+    else if (d == 0) {
+        x[0] = -b / 2 * a;
+    }
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let sum = 0;
+    
+    if (marks.length === 0) {
+        return 0;
+    } else if (marks.length > 5) {
+        console.log(`Аргументов больше 5`);
+        marks.splice(5);
+    }
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    let averageMark = sum / marks.length;
+    return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name,dateOfBirthday){ 
+    let result = '';
+    
+    if (new Date().getFullYear() - dateOfBirthday.getFullYear() > 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+    return result;
 }
