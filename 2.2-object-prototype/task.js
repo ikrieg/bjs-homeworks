7d65638899099922e21760e54d3c695a035c1ed0
Igor Sklyarov <i.sklyarov90@gmail.com>
2020-05-07 17:11:18 +0300

    add 2.2 solution

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,24 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined)
+        return null;
+    const sound = animal.sound;    
+    return sound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if (marks.length == 0)
+        return 0;
+    const average = marks.reduce(reducer) / marks.length;
+    const roundAverage = Math.round(average);    
+    return roundAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = +new Date();
+    const diff = now - +new Date(birthday);
+    let age = diff / 365.25 / 24 / 60 / 60 / 1000;
+    if (age > 18)
+        return true;
+    return false;    
 }
